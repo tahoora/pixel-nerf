31d13e818ddb89f655cc4495f1b5f1377781fd1f
tahoora ahmadyan <tahoora74@ymail.com>
2022-09-11 00:13:37 +0430

    Update gen_video.py

--- eval/gen_video.py
+++ eval/gen_video.py
@@ -44,7 +44,7 @@ def extra_args(parser):
     parser.add_argument(
         "--elevation",
         type=float,
-        default=-30.0,
+        default=-60.0,
         help="Elevation angle (negative is above)",
     )
     parser.add_argument(
